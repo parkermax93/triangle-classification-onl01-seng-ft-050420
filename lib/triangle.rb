2859123a7fe522916f236f7aa83a43fccaaedbb9
Parker Williamson <parker.williamson@gmail.com>
2020-05-21 20:43:39 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :sidea, :sideb, :sidec
+  
+  def initialize (sidea, sideb, sidec)
+    @sidea= sidea
+    @sideb=sideb
+    @sidec=sidec
+  end
+   
+  def kind 
+    if sidea <=0 || sideb <= 0 || sidec <= 0 || (sidea + sideb) <= sidec || (sidea+ sidec) <= sideb || (sideb+sidec) <= sidea
+      raise TriangleError
+    elsif  sidea == sideb && sidea == sidec 
+      :equilateral
+    elsif sideb== sidec && sideb != sidea && sidec !=sidea
+      :isosceles
+    elsif sidea == sideb && sidea != sidec && sideb !=sidec
+      :isosceles
+    elsif sidea== sidec && sideb !=sidea && sidec != sideb
+       :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  
+  
+  
+  class TriangleError < StandardError
+  end
+  
+  
+  
+  
 end
